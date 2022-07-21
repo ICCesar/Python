5cbf7d1b8194cc3ebd38c6c08745276f8e34f9fa
ICCesar <ICCesar@users.noreply.github.com>
2022-07-21 13:30:23 -0600

    Update codecademy_project_02_insurance.py

--- codecademy/codecademy_project_02_insurance.py
+++ codecademy/codecademy_project_02_insurance.py
@@ -1,11 +1,11 @@
 # Create calculate_insurance_cost() function below: 
-def calculate_insurance_cost(age, sex, bmi, num_of_children, smoker):
+def calculate_insurance_cost(name, age, sex, bmi, num_of_children, smoker):
   estimated_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
-  print("The estimated insurance cost for this person is " + str(estimated_cost) + " dollars.")
+  print("The estimated insurance cost for " + name + " is " + str(estimated_cost) + " dollars.")
   return estimated_cost
 
 # Estimate Maria's insurance cost
-maria_insurance_cost = calculate_insurance_cost(28, 0, 26.2, 3, 0)
+maria_insurance_cost = calculate_insurance_cost(name = "Maria", age = 28, sex = 0, bmi = 26.2, num_of_children = 3, smoker = 0)
 
 # Estimate Omar's insurance cost 
-omar_insurance_cost = calculate_insurance_cost(35, 1, 22.2, 0, 1)
+omar_insurance_cost = calculate_insurance_cost(name = "Omar", age = 35, sex = 1, bmi = 22.2, num_of_children = 0, smoker = 1)
