02a0050de0744457f94a406a75d1980a031d422e
ICCesar <ICCesar@users.noreply.github.com>
2022-08-03 13:07:55 -0600

    Create codecademy_project_Sals_Shipping.py

---
+++ codecademy/codecademy_project_Sals_Shipping.py
@@ -0,0 +1,36 @@
+# 1
+weight = 41.5
+
+# 2 
+# Ground Shipping
+if weight <= 2:
+  cost_ground = weight * 1.5 + 20
+elif weight <= 6:
+  cost_ground = weight * 3.00 + 20
+elif weight <= 10:
+  cost_ground = weight * 4.00 + 20
+else:
+  cost_ground = weight * 4.75 + 20
+
+# 3
+print("Ground Shipping $", cost_ground)
+
+# 4
+cost_ground_premium = 125.00
+
+# 5
+print("Ground Shipping Premium $", cost_ground_premium)
+
+# 6
+# Drone Shipping
+if weight <= 2:
+  cost_drone = weight * 4.5
+elif weight <= 6:
+  cost_drone = weight * 9.00
+elif weight <= 10:
+  cost_drone = weight * 12.00
+else:
+  cost_drone = weight * 14.25
+
+# 7
+print("Drone Shipping: $", cost_drone)
